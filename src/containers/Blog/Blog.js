33b790f2962000e0bebbccfcb9f5ff2876fc4717
keithchasen <keithchasen89@gmail.com>
2019-10-27 14:16:35 +0200

    handling errors on blog page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -27,6 +28,9 @@ class Blog extends Component {
                 })
                 // console.log(response)
             })
+            .catch(err => {
+                this.setState({error: true})
+            })
     }
 
     postSelectedHandler = id => {
@@ -34,15 +38,18 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts
-            .map(
-                post => <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts
+                .map(
+                    post => <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
                 )
+        }
 
         return (
             <div>
