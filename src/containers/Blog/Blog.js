70e6f1f8e5f987c870c0a12e45784efa21c0399a
keithchasen <keithchasen89@gmail.com>
2019-10-30 15:57:09 +0200

    lazy loading of components only when the appropriate route was used

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,12 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
 
 import './Blog.css';
 import Posts from './Posts/Posts'
-import NewPost from "./NewPost/NewPost";
+
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from "./NewPost/NewPost";
+const AsyncNewPost = asyncComponent(() => {
+    return import("./NewPost/NewPost")
+})
 
 class Blog extends Component {
 
@@ -47,7 +52,8 @@ class Blog extends Component {
                 </div>
 
                 <Switch>
-                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null }
+                    {/*{this.state.auth ? <Route path='/new-post' component={NewPost} /> : null }*/}
+                    {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null }
                     <Route path='/posts/' component={Posts} />
                     {/*<Redirect from="/" to="/posts" />*/}
                     <Route render={() => <h1>Not Found</h1>} />
